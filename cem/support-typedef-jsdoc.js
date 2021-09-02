ae55a61b23e4f211bf3ae9247f11d7a3622792d2
Galimede <mat.degand@outlook.fr>
2021-09-02 14:18:28 +0200

    --wip-- [skip ci]

--- cem/support-typedef-jsdoc.js
+++ cem/support-typedef-jsdoc.js
@@ -6,26 +6,20 @@ function convertFile (ts, filename, interfaceName) {
 }
 
 function delint (ts, node, interfaceName) {
-  const interfaces = [];
   const st = node?.statements.find((st) => st.name.getText() === interfaceName);
-  interfaces.push(`
-      interface ${st?.name.getText()} {
-        ${st?.members?.map((mb) => `${mb.name.getText()}: ${mb.type.getText()}`).join('\n')}
-      }
-    `);
+  if (st?.type?.types) {
+    return '```js\n'
+      + 'type ' + st.name.getText() + ': '
+      + (st?.type?.types.map((type) => `'${type.literal.text}'`).join(' | ') ?? '')
+      + '\n```';
+  }
+  return '```js\n'
+    + 'interface ' + st?.name.getText() + ' { \n'
+    + st?.members?.map((mb) => `${mb?.name.getText()}: ${mb?.type.getText()},`).join('\n')
+    + '\n}\n ```';
 
-  return interfaces;
 }
 
-/**
- * CEM analyzer plugin: support-cssdisplay-jsdoc
- *
- * This plugin adds support for the @cssdisplay JSDoc tag.
- * This JSDoc tag helps author to document the default display of a custom element.
- *
- * This plugin will add a custom field "cssDisplay" to the CEM.
- * This plugin will also add this info at the beginning of the description, just after the first empty line.
- */
 export default function supportTypedefJsdoc () {
   return {
     name: 'support-typedef-jsdoc',
@@ -39,28 +33,17 @@ export default function supportTypedefJsdoc () {
             jsDoc?.tags
               ?.filter((tag) => tag.tagName.getText() === 'typedef')
               ?.forEach((tag) => {
-                console.log(tag);
 
                 const typeDefDisplay = tag.name.getText();
-
-                const classDeclaration = moduleDoc.declarations.find((declaration) => declaration.name === node.name.getText());
-                classDeclaration.typeDef = typeDefDisplay;
 
                 const url = new URL(moduleDoc.path, import.meta.url);
                 const typeUrl = new URL('types.d.ts', url.toString().split('/cem').join(''));
                 const converted = convertFile(ts, typeUrl.pathname, typeDefDisplay);
 
-                // const displayLine = `🎨 default typedef display: \`${typeDefDisplay}\` ${tag.typeExpression.type.argument?.literal.getText()} ${converted[2]}`;
-                const displayLine = converted;
-                const descriptionLines = classDeclaration.description.split('\n');
-                const firstEmptyLineIndex = descriptionLines.findIndex((line) => line === '');
-                if (firstEmptyLineIndex === -1) {
-                  descriptionLines.push('', displayLine);
-                }
-                else {
-                  descriptionLines.splice(firstEmptyLineIndex, 0, '', displayLine);
-                }
-                classDeclaration.description = descriptionLines.join('\n');
+                const declaration = moduleDoc.declarations.find((declaration) => declaration.name === node.name.getText());
+
+                declaration.description = declaration.description + '\n\n' + converted;
+
               });
           });
 
